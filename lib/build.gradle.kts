ddaf23f6c7fb96282de8a66a1b402758389dfcb0
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-24 17:27:16 +0300

    maven

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,6 +8,22 @@ plugins {
     id("kotlin-android")
 }
 
+fun getVersionName(variant: com.android.build.gradle.api.LibraryVariant): String {
+    return when (variant.buildType.name) {
+        "release" -> "${Version.Application.name}-${variant.flavorName}"
+        else -> "${Version.Application.name}-${variant.name}"
+    }
+}
+
+fun getVersion(variant: com.android.build.gradle.api.LibraryVariant): String {
+    return "${getVersionName(variant)}-${Version.Application.code}"
+}
+
+fun getOutputFileName(variant: com.android.build.gradle.api.LibraryVariant, extension: String): String {
+    check(extension.isNotEmpty())
+    return "${rootProject.name}-${getVersion(variant)}.$extension"
+}
+
 android {
     namespace = "sp.ax.jc.clicks"
     compileSdk = Version.Android.compileSdk
@@ -36,17 +52,29 @@ android {
         val variant = this
         val output = variant.outputs.single()
         check(output is com.android.build.gradle.internal.api.LibraryVariantOutputImpl)
-        val versionName = when (variant.buildType.name) {
-            "release" -> "${Version.Application.name}-${variant.flavorName}"
-            else -> "${Version.Application.name}-${variant.name}"
-        }
-        output.outputFileName = "${rootProject.name}-$versionName-${Version.Application.code}.aar"
+        output.outputFileName = getOutputFileName(variant, "aar")
         afterEvaluate {
             tasks.getByName<JavaCompile>("compile${variant.name.capitalize()}JavaWithJavac") {
                 targetCompatibility = Version.jvmTarget
             }
             tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compile${variant.name.capitalize()}Kotlin") {
                 kotlinOptions.jvmTarget = Version.jvmTarget
+            }
+            task("assemble${variant.name.capitalize()}Pom") {
+                doLast {
+                    val target = File(buildDir, "libs").let {
+                        it.mkdirs()
+                        File(it, getOutputFileName(variant, "pom"))
+                    }
+                    if (target.exists()) target.delete()
+                    val text = MavenUtil.pom(
+                        groupId = Maven.groupId,
+                        artifactId = Maven.artifactId,
+                        version = getVersion(variant),
+                        packaging = "aar"
+                    )
+                    target.writeText(text)
+                }
             }
         }
     }
