ea89b9edff01afb40559ad6a9cf7a4d13c2efa34
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-24 13:40:17 +0300

    init app

---
+++ app/src/main/kotlin/sp/ax/clicks/sample/App.kt
@@ -0,0 +1,10 @@
+package sp.ax.clicks.sample
+
+import android.app.Application
+
+internal class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // todo
+    }
+}
---
+++ app/src/main/kotlin/sp/ax/clicks/sample/MainActivity.kt
@@ -0,0 +1,25 @@
+package sp.ax.clicks.sample
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+internal class MainActivity : AppCompatActivity() {
+    override fun onCreate(inState: Bundle?) {
+        super.onCreate(inState)
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+            ) {
+                // todo
+            }
+        }
+    }
+}
