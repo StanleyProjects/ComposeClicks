e27039fc2d7128056fcc70bbfa150141f86aa73f
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-26 14:49:59 +0300

    test coverage roboletric

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,4 +1,7 @@
 object Version {
+//    const val jacoco = "0.8.7"
+//    const val jacoco = "0.8.8"
+    const val jacoco = "0.8.9"
     const val jvmTarget = "11"
     const val kotlin = "1.7.10"
     const val ktlint = "0.49.0"
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,6 +6,7 @@ repositories {
 plugins {
     id("com.android.library")
     id("kotlin-android")
+    id("org.gradle.jacoco")
 }
 
 fun getVersionName(variant: com.android.build.gradle.api.LibraryVariant): String {
@@ -24,6 +25,40 @@ fun getOutputFileName(variant: com.android.build.gradle.api.LibraryVariant, exte
     return "${rootProject.name}-${getVersion(variant)}.$extension"
 }
 
+jacoco {
+    toolVersion = Version.jacoco
+}
+
+fun setCoverage(variant: com.android.build.gradle.api.LibraryVariant) {
+    val capitalize = variant.name.capitalize()
+    val taskUnitTest = tasks.getByName<Test>("test${capitalize}UnitTest")
+    val taskCoverageReport = task<JacocoReport>("test${capitalize}CoverageReport") {
+        dependsOn(taskUnitTest)
+        reports {
+            csv.required.set(false)
+            html.required.set(true)
+            xml.required.set(false)
+        }
+        sourceDirectories.setFrom(file("src/main/kotlin"))
+        val dirs = fileTree(File(buildDir, "tmp/kotlin-classes/" + variant.name))
+        classDirectories.setFrom(dirs)
+//        executionData(taskUnitTest)
+        executionData("${buildDir}/outputs/unit_test_code_coverage/${variant.name}UnitTest/${taskUnitTest.name}.exec")
+    }
+    task<JacocoCoverageVerification>("test${capitalize}CoverageVerification") {
+        dependsOn(taskCoverageReport)
+        violationRules {
+            rule {
+                limit {
+                    minimum = BigDecimal(0.96)
+                }
+            }
+        }
+        classDirectories.setFrom(taskCoverageReport.classDirectories)
+        executionData(taskCoverageReport.executionData)
+    }
+}
+
 android {
     namespace = "sp.ax.jc.clicks"
     compileSdk = Version.Android.compileSdk
@@ -31,12 +66,24 @@ android {
     testOptions {
         unitTests {
             isIncludeAndroidResources = true
+            all {
+                // https://stackoverflow.com/a/71834475/4398606
+                it.configure<JacocoTaskExtension> {
+                    isIncludeNoLocationClasses = true
+                    excludes = listOf("jdk.internal.*")
+                }
+            }
         }
     }
 
     defaultConfig {
         minSdk = Version.Android.minSdk
         manifestPlaceholders["appName"] = "@string/app_name"
+    }
+
+    buildTypes.getByName(testBuildType) {
+//        enableUnitTestCoverage = true
+        isTestCoverageEnabled = true
     }
 
     productFlavors {
@@ -60,6 +107,7 @@ android {
         check(output is com.android.build.gradle.internal.api.LibraryVariantOutputImpl)
         output.outputFileName = getOutputFileName(variant, "aar")
         afterEvaluate {
+            setCoverage(variant)
             tasks.getByName<JavaCompile>("compile${variant.name.capitalize()}JavaWithJavac") {
                 targetCompatibility = Version.jvmTarget
             }
@@ -90,5 +138,5 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:${Version.Android.compose}")
     testImplementation("org.robolectric:robolectric:4.10")
     testImplementation("androidx.compose.ui:ui-test-junit4:${Version.Android.compose}")
-    "${android.testBuildType}Implementation"("androidx.compose.ui:ui-test-manifest:${Version.Android.compose}")
+    "test${android.testBuildType.capitalize()}Implementation"("androidx.compose.ui:ui-test-manifest:${Version.Android.compose}")
 }
