a58cf296b5adc7a9e9357753d9e44aa3bd0784e5
Stanley Wintergreen <kepocnhh@gmail.com>
2023-07-12 13:50:11 +0300

    fix code style

--- lib/src/main/kotlin/sp/ax/jc/clicks/Click.kt
+++ lib/src/main/kotlin/sp/ax/jc/clicks/Click.kt
@@ -49,7 +49,7 @@ fun Modifier.onClick(
                         },
                     )
                 }
-        }
+        },
     )
 }
 
--- lib/src/main/kotlin/sp/ax/jc/clicks/Clicks.kt
+++ lib/src/main/kotlin/sp/ax/jc/clicks/Clicks.kt
@@ -55,7 +55,7 @@ fun Modifier.clicks(
                         },
                     )
                 }
-        }
+        },
     )
 }
 
