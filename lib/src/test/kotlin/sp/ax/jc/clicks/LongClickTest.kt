a6d438d98c379f6fcb609dbb2d2a613a5b0273cd
Stanley Wintergreen <kepocnhh@gmail.com>
2023-09-13 17:59:56 +0300

    test on long click

--- lib/src/test/kotlin/sp/ax/jc/clicks/LongClickTest.kt
+++ lib/src/test/kotlin/sp/ax/jc/clicks/LongClickTest.kt
@@ -1,7 +1,10 @@
 package sp.ax.jc.clicks
 
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -14,6 +17,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.util.concurrent.atomic.AtomicBoolean
 
 @RunWith(RobolectricTestRunner::class)
 internal class LongClickTest {
@@ -21,26 +25,149 @@ internal class LongClickTest {
     val rule = createComposeRule()
 
     @Test
-    fun clicksTest() {
-        var value = false
-        val tag = "clickable"
+    fun onLongClickDefaultTest() {
+        val value = AtomicBoolean(false)
+        val tag = "onLongClick:default"
         rule.setContent {
             Box(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .testTag(tag)
-                    .onLongClick {
-                        value = !value
+                    .onLongClick(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = LocalIndication.current,
+                    ) {
+                        value.set(!value.get())
                     },
             )
         }
-        assertFalse(value)
+        onClickEnabledAssert(value = value, tag = tag)
+    }
+
+    @Test
+    fun onLongClickDefaultEnabledTest() {
+        val value = AtomicBoolean(false)
+        val tag = "onLongClick:enabled"
+        rule.setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testTag(tag)
+                    .onLongClick(
+                        enabled = true,
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = LocalIndication.current,
+                    ) {
+                        value.set(!value.get())
+                    },
+            )
+        }
+        onClickEnabledAssert(value = value, tag = tag)
+    }
+
+    @Test
+    fun onLongClickDefaultDisabledTest() {
+        val value = AtomicBoolean(false)
+        val tag = "onLongClick:disabled"
+        rule.setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testTag(tag)
+                    .onLongClick(
+                        enabled = false,
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = LocalIndication.current,
+                    ) {
+                        value.set(!value.get())
+                    },
+            )
+        }
+        onClickDisabledAssert(value = value, tag = tag)
+    }
+
+    @Test
+    fun onLongClickTest() {
+        val value = AtomicBoolean(false)
+        val tag = "onLongClick"
+        rule.setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testTag(tag)
+                    .onLongClick {
+                        value.set(!value.get())
+                    },
+            )
+        }
+        onClickEnabledAssert(value = value, tag = tag)
+    }
+
+    @Test
+    fun onLongClickEnabledTest() {
+        val value = AtomicBoolean(false)
+        val tag = "onLongClick:enabled"
+        rule.setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testTag(tag)
+                    .onLongClick(enabled = true) {
+                        value.set(!value.get())
+                    },
+            )
+        }
+        onClickEnabledAssert(value = value, tag = tag)
+    }
+
+    @Test
+    fun onLongClickDisabledTest() {
+        val value = AtomicBoolean(false)
+        val tag = "onLongClick:disabled"
+        rule.setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .testTag(tag)
+                    .onLongClick(enabled = false) {
+                        value.set(!value.get())
+                    },
+            )
+        }
+        onClickDisabledAssert(value = value, tag = tag)
+    }
+
+    private fun onClickEnabledAssert(
+        value: AtomicBoolean,
+        tag: String,
+    ) {
+        assertFalse(value.get())
+        @Suppress("IgnoredReturnValue")
         rule.onNodeWithTag(tag).performTouchInput {
             longClick()
         }
-        assertTrue(value)
+        assertTrue(value.get())
+        @Suppress("IgnoredReturnValue")
         rule.onNodeWithTag(tag).performTouchInput {
             longClick()
         }
-        assertFalse(value)
+        assertFalse(value.get())
+    }
+
+    private fun onClickDisabledAssert(
+        value: AtomicBoolean,
+        tag: String,
+    ) {
+        assertFalse(value.get())
+        @Suppress("IgnoredReturnValue")
+        rule.onNodeWithTag(tag).performTouchInput {
+            longClick()
+        }
+        assertFalse(value.get())
+        @Suppress("IgnoredReturnValue")
+        rule.onNodeWithTag(tag).performTouchInput {
+            longClick()
+        }
+        assertFalse(value.get())
     }
 }
