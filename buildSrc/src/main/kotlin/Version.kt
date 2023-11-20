c819ebf7b822b8af59c3f79968821260892d8647
Stanley Wintergreen <kepocnhh@gmail.com>
2023-11-20 21:53:31 +0300

    jvm target -> 17

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,7 +2,7 @@ object Version {
     const val detekt = "1.22.0"
     const val dokka = "1.8.10"
     const val jacoco = "0.8.9"
-    const val jvmTarget = "11"
+    const val jvmTarget = "17"
     const val kotlin = "1.9.10"
     const val ktlint = "0.49.1"
 
